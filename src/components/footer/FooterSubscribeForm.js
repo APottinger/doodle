a4665ee33446121c087436f8e1b2ac8f0c40d9c6
Allen Pottinger <allenpottinger@gmail.com>
2021-12-24 15:04:15 -0500

    creates Form component sub form

---
+++ src/components/footer/FooterSubscribeForm.js
@@ -0,0 +1,13 @@
+
+const FooterSubscribeForm = () => {
+    return (
+        <div>
+            <form>
+                <input type="email" name="email" placeholder="email" />
+                <input type='submit' />
+            </form>
+        </div>
+    )
+}
+
+export default FooterSubscribeForm
