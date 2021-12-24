039174dcf0e2557fa6ca1c48e591c6617b54cc25
Allen Pottinger <allenpottinger@gmail.com>
2021-12-24 15:05:24 -0500

    creates Footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import FooterNav from './FooterNav';
+import FooterSocialMedia from './FooterSocialMedia';
+import FooterSubscribeForm from './FooterSubscribeForm';
+const Footer = () => {
+    return (
+        <div>
+            <h1>doodle</h1>
+            <FooterSocialMedia />
+            <FooterSubscribeForm />
+            <FooterNav />
+        </div>
+    )
+}
+
+export default Footer
