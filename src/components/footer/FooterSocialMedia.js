9c1742dc9a2022a054928cd3fb75e1183ee733a9
Allen Pottinger <allenpottinger@gmail.com>
2021-12-24 15:04:46 -0500

    adds social media nav to Footer Component

---
+++ src/components/footer/FooterSocialMedia.js
@@ -0,0 +1,17 @@
+import { FaInstagram, FaFacebook, FaGmail, FaSnapchat, FaTwitter } from "react-icons/fa";
+
+const FooterSocialMedia = () => {
+    return (
+        <div> 
+            <ul>
+                <li><a href="#"><FaInstagram /></a></li>
+                <li><a href="#"><FaFacebook /></a></li>
+                <li><a href="#"><FaSnapchat /></a></li>
+                <li><a href="#"><FaTwitter /></a></li>
+                
+            </ul>
+        </div>
+    )
+}
+
+export default FooterSocialMedia
