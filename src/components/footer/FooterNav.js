034d39db39cd4a51f2afe66f09965520e98a9bab
Allen Pottinger <allenpottinger@gmail.com>
2021-12-24 15:05:09 -0500

    adds navlinks to Footer Component

---
+++ src/components/footer/FooterNav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const FooterNav = () => {
+    return (
+        <div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Terms</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+            </ul>
+
+            <p>doodle&copy; 2021</p>
+        </div>
+    )
+}
+
+export default FooterNav
