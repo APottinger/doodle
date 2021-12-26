6ce5c86df254b78aa4aaa75a8a63ca8448cb7c61
Allen Pottinger <allenpottinger@gmail.com>
2021-12-26 16:04:38 -0500

    refactor: create HeaderLeftNav

---
+++ src/components/header/HeaderArticle.js
@@ -0,0 +1,9 @@
+const HeaderArticle = () => {
+    return (
+        <div style={{backgroundImage: `url("https://www.uco.edu/cfad/images/art/art-header.jpg")`, height: "500rem", width: '100%'}}  >
+            {/* <img src="https://www.uco.edu/cfad/images/art/art-header.jpg" height="500rem" width='100%' alt="" /> */}
+        </div>
+    )
+}
+
+export default HeaderArticle
