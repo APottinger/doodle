9e977fe42ca728fa86c373a3e4404389d84144fe
Allen Pottinger <allenpottinger@gmail.com>
2021-12-26 16:05:14 -0500

    refactor: imports _Header components

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,6 @@
-
+import HeaderArticle from './HeaderArticle';
+import HeaderLeftNav from './HeaderLeftNav';
+import HeaderRightNav from './HeaderRightNav';
 
 const Header = () => {
 
@@ -6,18 +8,9 @@ const Header = () => {
     return (
         <div>
             <h1>doodle</h1>
-            <nav className="nav" style={{display: 'flex'}}>
-                <ul style={{display: 'flex'}}>
-                    <li style={{listStyle: 'none', padding: '2rem'}}><a href="#">Home</a></li>
-                    <li style={{listStyle: 'none', padding: '2rem'}}><a href="#">Category</a></li>
-                    <li style={{listStyle: 'none', padding: '2rem'}}><a href="#">Contact</a></li>
-                </ul>
-                <ul style={{display: 'flex'}}>
-                    <li style={{listStyle: 'none', padding: '2rem'}}><a href="#">Join Now</a></li>
-                    <li style={{listStyle: 'none', padding: '2rem'}}><a href="#">Login</a></li>
-                </ul>
-            </nav>
-            <img src="https://www.uco.edu/cfad/images/art/art-header.jpg" height="500rem" width='100%' alt="" />
+            <HeaderLeftNav />
+            <HeaderRightNav />
+            <HeaderArticle />
         </div>
     )
 }
