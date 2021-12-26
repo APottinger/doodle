f5c8050f649f135ec1f4b557dac68ceef59afcc1
Allen Pottinger <allenpottinger@gmail.com>
2021-12-26 16:04:19 -0500

    refactor: create HeaderRightNav

---
+++ src/components/header/HeaderRightNav.js
@@ -0,0 +1,12 @@
+const HeaderRightNav = () => {
+    return (
+        <div>
+            <ul style={{display: 'flex'}}>
+                    <li style={{listStyle: 'none', padding: '2rem'}}><a href="#">Join Now</a></li>
+                    <li style={{listStyle: 'none', padding: '2rem'}}><a href="#">Login</a></li>
+                </ul>
+        </div>
+    )
+}
+
+export default HeaderRightNav
