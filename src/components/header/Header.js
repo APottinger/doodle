faf828271d628b4442ba0f51793660e0c8214041
Allen Pottinger <allenpottinger@gmail.com>
2021-12-27 20:39:15 -0500

    refactors to @mui

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,20 @@
-import HeaderArticle from './HeaderArticle';
+import { AppBar, Typography, Container } from '@mui/material';
+import { Camera }  from '@mui/icons-material';
 import HeaderLeftNav from './HeaderLeftNav';
 import HeaderRightNav from './HeaderRightNav';
+import HeaderArticle from './HeaderArticle'
 
 const Header = () => {
 
-
     return (
         <div>
-            <h1>doodle</h1>
-            <HeaderLeftNav />
-            <HeaderRightNav />
+            <AppBar position='relative' >
+                <Typography variant='h4'><Camera />doodle</Typography>
+                <Container maxWidth='md'>
+                    <HeaderLeftNav />
+                    <HeaderRightNav />
+                </Container>
+            </AppBar>
             <HeaderArticle />
         </div>
     )
