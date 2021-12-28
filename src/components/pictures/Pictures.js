e94c494c3807395fa912844c3ebfec65943860ee
Allen Pottinger <allenpottinger@gmail.com>
2021-12-27 20:40:29 -0500

    adds grid to map of pictures

--- src/components/pictures/Pictures.js
+++ src/components/pictures/Pictures.js
@@ -1,10 +1,22 @@
-import Picture from './picture/Picture'
+import { Typography, Grid } from '@mui/material';
+import Picture from './picture/Picture';
+import useStyles from './styles';
 
 const Pictures = ({ pictures, deleteBtn }) => {
+
+    /* const classes = useStyles() */
     return (
-        <div>
-            {pictures.map(picture => <Picture key={pictures.id} picture={picture} deleteBtn={deleteBtn}/>)}
-        </div>
+        <>
+            <Typography variant='h2'>
+                Featured Photos
+            </Typography>
+                <Grid container spacing={2}>                    
+                    {pictures.map(picture => <Grid item xs={12} sm={6} md={4}> 
+                    <Picture key={pictures.id} picture={picture} deleteBtn={deleteBtn}/> 
+                    </Grid>)}
+                </Grid>
+            
+        </>
     )
 }
 
