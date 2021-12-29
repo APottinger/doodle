e542e51ade1c748a9d5937208cd4273cd656fe8f
Allen Pottinger <allenpottinger@gmail.com>
2021-12-29 14:55:00 -0500

    adds Grid

--- src/components/header/HeaderArticle.js
+++ src/components/header/HeaderArticle.js
@@ -4,24 +4,23 @@ import { Grid, Typography, Container, Button } from '@mui/material';
 const HeaderArticle = () => {
     return (
         <main>
-            <Container maxWidth='lg' justify='center'>
-                <Typography variant='h1'>We Believe</Typography>
-                <Typography variant='p' paragraph>Art Can Change the World. Art can totally transform what we think we know about the world and what we dont know, we can imagine through art. What we think we know
+            <Container maxWidth='md' justify='center'>
+                <Typography variant='h1' sx={{textAlign: 'center'}}>We Believe</Typography>
+                <Typography variant='p' paragraph sx={{textAlign: 'center'}}>Art Can Change the World. Art can totally transform what we think we know about the world and what we dont know, we can imagine through art. What we think we know
                 we dont and even what we think we do know it is only like a leaf blowing in the wind. It's meaningless outside the ambigious and abstraction that is art itself - defined...
                 </Typography>
             </Container>
-
-            <Container maxWidth='sm'>
-                <Grid container spacing={3} justify='center'>
-                    <Grid item >
-                        <Button variant='contained' color='primary'>See All Photos</Button>
-                    </Grid>
-                    <Grid item >
-                    <Button variant='outlined' color='primary'>Add Your Photos</Button>
-                    </Grid>
+            
+            <Grid container spacing={3} sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Grid item >
+                    <Button variant='contained' color='primary'>See All Photos</Button>
                 </Grid>
+                <Grid item >
+                <Button variant='outlined' color='primary'>Add Your Photos</Button>
+                </Grid>
+            </Grid>
                 {/* <HeaderForm /> */}
-            </Container>
+            
         </main>
     )
 }
