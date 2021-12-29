d30311b79394cb8bbe10be44f38f91e7a2edb716
Allen Pottinger <allenpottinger@gmail.com>
2021-12-29 14:55:15 -0500

    creates header form

---
+++ src/components/header/HeaderForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderForm = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" placeholder="search the gallery..." name="" />
+                <input type='submit' />
+            </form>
+        </div>
+    )
+}
+
+export default HeaderForm
