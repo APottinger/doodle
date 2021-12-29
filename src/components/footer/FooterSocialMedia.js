8c07bb2edf249f25401230c6397c0f9ea4d4287f
Allen Pottinger <allenpottinger@gmail.com>
2021-12-29 14:54:25 -0500

    adds bottom navbar to social media footer

--- src/components/footer/FooterSocialMedia.js
+++ src/components/footer/FooterSocialMedia.js
@@ -1,16 +1,31 @@
-import { FaInstagram, FaFacebook, FaGmail, FaSnapchat, FaTwitter } from "react-icons/fa";
-
+import { FaInstagram, FaFacebook, FaSnapchat, FaTwitter } from "react-icons/fa";
+import { BottomNavigation, BottomNavigationAction } from '@mui/material'
 const FooterSocialMedia = () => {
     return (
-        <div> 
-            <ul>
-                <li><a href="#"><FaInstagram /></a></li>
-                <li><a href="#"><FaFacebook /></a></li>
-                <li><a href="#"><FaSnapchat /></a></li>
-                <li><a href="#"><FaTwitter /></a></li>
-                
-            </ul>
-        </div>
+        
+        <BottomNavigation showLabels sx={{ width: 1, backgroundColor: '#525252'}}>
+            <BottomNavigationAction
+                label={<a href="#"></a>}
+                icon={<FaInstagram size={70} />} 
+            /> 
+        
+            <BottomNavigationAction
+                label={<a href="#"></a>}
+                icon={<FaFacebook size={70} color='#CC2D90'/>} 
+            />
+        
+            <BottomNavigationAction
+                label={<a href="#"></a>}
+                icon={<FaSnapchat size={70} color='#CC2D90' />} 
+            />
+        
+            <BottomNavigationAction
+                label={<a href="#"></a>}
+                icon={<FaTwitter size={70} color='#CC2D90' />} 
+            />
+        </BottomNavigation>
+        
+        
     )
 }
 
